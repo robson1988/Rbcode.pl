f39e42dc290d208f5d0f9087cfdd899d1bb306d8
RBcode <40271285+robson1988@users.noreply.github.com>
2020-10-13 21:17:33 +0200

    Add files via upload

---
+++ includes/Mailing/zapytanie-klienta.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<title>Profesjonalne i responsywne strony internetowe | Rbcode.pl</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+</head>
+<body style="font-family:'verdana';">
+
+<!--PREHEADER-->
+<div style="display:none; font-size:1px; color:#e5e5e5; line-height:1px; font-family:Verdana; max-height:0px; max-width:0px; opacity:0; overflow:hidden;">Zdrowe, smaczne posiłki, zawsze świeże i na czas | Fitbox</div>
+
+<table style="width:720px; background-color:#fff; font-size:14px; font-weight:300;margin:auto; padding:0;border-collapse: collapse;">
+<tr>
+	<td style="padding:0; background-color:#fff;">
+		<table style="width:720px;padding:0; border-collapse: collapse;">
+			<!-- Logo-->
+				<tr>
+					<td><img src="https://i.ibb.co/wN56B4C/logo-medvisor.png" alt="Maseczki medyczne | Medvisor" style="width:120px;height:auto;display:block;margin:auto;"></td>
+				</tr>
+				<tr>
+					<td style="color:#f07d00;padding:5px 0;text-align:center;">
+						<a href="https://medvisor.pl" style="color:#9B4493;text-decoration:none;" target="_blank">www.medvisor.pl</a>
+					</td>
+				</tr>
+		</table>
+		<table style="padding:0; background-color:#fcfcfc; border-collapse:collapse;border-spacing:0px;"><!-- Zawartość główna-->
+			<tr>
+				<td style="padding:1em;">
+					<table style="padding:0;border-collapse:collapse;">
+						<tr style="background-color:lightgrey;">
+							<td style="width:240px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke ;">
+								<p style="font-size:13px; font-weight:bold;">Imię i nazwisko</p>
+							</td>
+							<td style="width:480px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;">%name%
+							</td>
+						</tr>
+
+						<tr>
+							<td style="width:240px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">
+								<p style="font-size:13px; font-weight:bold;">E-mail</p>
+							</td>
+							<td style="width:480px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">%email%
+							</td>
+						</tr>
+
+						<tr style="background-color:lightgrey;">
+							<td style="width:240px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">
+								<p style="font-size:13px; font-weight:bold;">Telefon</p>
+							</td>
+							<td style="width:480px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">%phone%
+							</td>
+						</tr>
+
+						<tr>
+							<td style="width:240px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">
+								<p style="font-size:13px; font-weight:bold;">Tytuł zapytania</p>
+							</td>
+							<td style="width:480px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">%subject%
+							</td>
+						</tr>
+
+						<tr style="background-color:lightgrey;">
+							<td style="width:240px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">
+								<p style="font-size:13px; font-weight:bold;">Treść zapytania</p>
+							</td>
+							<td style="width:480px;padding:0;color:#1b1b1b; font-weight:400; text-align: center;border:1px solid whitesmoke;border-top:0;">%message%
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
