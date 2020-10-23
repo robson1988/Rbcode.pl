87ce87e79d09f11ec38fcd09e002d68ace100eff
robson1988 <robert,bajoo@gmail.com>
2020-10-23 15:19:21 +0200

    nowa strona pozycjonowanie

---
+++ pozycjonowanie.php
@@ -0,0 +1,228 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+	<title>Pozycjonowanie stron internetowych | Audyt SEO | Rbcode.pl</title>
+	<meta name="description" content="Skuteczne pozycjonowanie stron internetowych i optymalizacja stron www. Zapytaj o audyt SEO twojej strony internetowej ✅ Pozycjonowanie stron Stalowa Wola!">
+	<meta name="keywords" content="pozycjonowanie stalowa wola, pozycjonowanie stron stalowa wola, pozycjonowanie stron,pozycjonowanie, słowa kluczowe, pozycjonowanie strony w google, pozycjonowanie strony, optymalizacja strony, pozycja w google, pozycjonowanie seo, skuteczne pozycjonowanie stron, usługi pozycjonowania, pozycjonowanie i optymalizacja stron www, pozycjonowanie stron marketing internetowy, optymalizacja i pozycjonowanie stron internetowych, pozycjonowanie strony sklepu internetowego, na czym polega pozycjonowanie stron internetowych, ile kosztuje pozycjonowanie strony w google, ile kosztuje pozycjonowanie sklepu internetowego, ile kosztuje pozycjonowanie strony internetowej, skuteczne pozycjonowanie stron internetowych, ceny pozycjonowania stron internetowych, pozycjonowanie stron internetowych w wyszukiwarkach, profesjonalne pozycjonowanie stron internetowych, tworzenie i pozycjonowanie stron internetowych, tworzenie stron internetowych i pozycjonowanie">
+	<link rel="canonical" href="https://rbcode.pl/pozycjonowanie-stron-internetowych">
+	<script type="application/ld+json">
+		 {
+				"@context": "https://schema.org/",
+				"@type": "WebPage",
+				"@id": "#WebPage",
+				"url": "https://rbcode.pl/pozycjonowanie-stron-internetowych",
+				"name": "Pozycjonowanie stron internetowych i optymalizacja stron www pod kątem SEO | Zapytaj o audyt SEO | Rbcode.pl",
+				"description": "Pozycjonowanie stron internetowych Stalowa Wola, Nisko, Tarnobrzeg i Sandomierz. Wykonamy audyt twojej strony a następnie przeprowadzimy skuteczną optymalizację i pozycjonowanie strony internetowej lub sklepu internetowego. Zapraszamy do kontaktu!",
+				"inLanguage": "pl-PL",
+				"isPartOf": {
+					"@context": "https://schema.org/",
+					"@type": "WebSite",
+					"@id": "#WebSite",
+					"url": "https://rbcode.pl/",
+					"name": "Tworzenie stron internetowych Stalowa Wola, pozycjonowanie stron Stalowa Wola oraz kodowanie mailingów reklamowych | Rbcode.pl",
+					"description": "Profesjonalne strony internetowe Stalowa Wola. Pozycjonowanie stron internetowych, kodowanie mailingów. Tworzymy nowoczesne i responsywne strony internetowe zoptymalizowane pod wyszukiwarkę Google. Strony internetowe tworzymy zgodnie z najlepszymi praktykami. Nasze strony www są bezpieczne i szybkie. Pozycjonowanie stron internetowych stworzonych przez nas już od początku jest łatwiejsze! Optymalizujemy działające strony internetowe w celu poprawy widoczności w wynikach wyszukiwania! Prowadzisz kampanie e-mail marketingowe i potrzebujesz zakodować szablon wiadomości e-mail? Wykonamy kodowanie mailingów zgodne z twoimi wytycznymi lub według dostarczonego szablonu psd. Zapraszamy do kontaktu z nami!",
+					"inLanguage": "pl-PL"
+				}
+			}
+	</script>
+	<?php include_once "includes/main-config-inc.php"; ?>
+	<?php include_once "includes/navigation-inc.php"; ?>
+
+<!-- === Page Content === -->
+<div class="container-fluid p-0">
+    <!-- === Banner Section  === -->
+    	<div class="row m-0">
+    		<div class="col-12 baner-section-seo">
+    			<div class="baner-inside" title="Nowoczesne i profesjonalne strony internetowe">
+    				<div class="col-lg-4 col-md-12 col-sm-12 p-0">
+    						<p class="our-partners">optymalizacja i pozycjonowanie stron www</p>
+    						<h1 class="font-kenit"><span class="one-letter">P</span>ozycjonowanie stron i<span class="one-letter">n</span>ternetowych</h1>
+    				</div>
+    		</div>
+    	</div>
+    </div> <!-- === Banner Section End === -->
+    <!-- === Section One === -->
+    <div class="container mx-auto">
+    	<div class="row">
+    		<div class="col-12 seo-page-section">
+    			<div class="center-content my-auto">
+    			 <h2 class="section-title font-kanit"><span class="one-letter">C</span>o to jest pozycjonowanie?</h2>
+    			 <p class="pl-3 pr-3  font-merriweather">
+             <strong>Pozycjonowanie stron internetowych</strong> (ang. Search Engine Optimization) to zestaw działań, których celem jest zwiększenie widoczności strony internetowej w darmowych wynikach wyszukiwania na określone słowa kluczowe. Zajmowanie pierwszych miejsc w wynikach wyszukiwania pozwala na zwiększenie liczby odwiedzin strony a tym samym wzrost dochodu ze sprzedaży produktów lub usług.
+    			</p>
+    		</div>
+    	 </div>
+    	</div>
+     </div> <!-- === Section End === -->
+     <!-- === Section Two === -->
+     <div class="container mx-auto">
+      <div class="row">
+        <div class="col-12 seo-page-section">
+          <div class="center-content my-auto">
+           <h2 class="section-title font-kanit">Jakie <span class="one-letter">k</span>orzyści wynikają z pozycjonowania strony</h2>
+           <p class="pl-3 pr-3  font-merriweather">
+						 Dzięki wysokim pozycjom w wynikach wyszukiwania zwiększamy ruch w witrynie i ilość odwiedzających ją użytkowników. Docieramy do większej liczby potencjalnych klientów i zwiększamy ilość zakupionych produktów oraz zapytań dotyczących oferowanych usług.<br>
+						 Skuteczne pozycjonowanie stron internetowych zwiększa pojawianie się witryny na większą liczbę słów kluczowych. Pozwala również wyprzedzić konkurencję w swojej branży. Budując zaufanie do swojego serwisu użytkownicy zaczynają utożsamiać naszą stronę z produktami lub oferownymi przez nas usługami. Zwiększamy swoją rozpoznawalność a co za idzie rozwijamy swój biznes oraz tworzymy własną markę. Klienci kojarząć markę mogą częściej podejmować decyzję o skorzystaniu z Twojej oferty.
+          </p>
+        </div>
+       </div>
+      </div>
+			<div class="row">
+				<div class="col-lg-8 col-sm-12 card">
+		 			<img src="images/ctr w zaleznosci od pozycji w wynikach wyszukwiania.jpg" class="img-fluid lazyload" alt="skuteczne pozycjonowanie stron internetowych - korzyści" title="Korzyści z pozycjonowania stron internetowych">
+		 	 </div>
+		   <div class="col-lg-4 col-sm-12 card horizontal-align">
+				 <div class="center-content my-auto">
+			    <h3 class="section-title font-kanit"><span class="one-letter">C</span>TR w zależności od pozycji</h3>
+					<hr class="line">
+				    <p class="pl-3 pr-3 font-merriweather">
+							Na zamieszczonym wykresie możemy zaobserwować, że największy CTR wsród użytkowników mają pozycje w TOP3. Na desktopie w sumie klikalność w TOP3 wynosi 64,08%, a na mobile 57,89%.<br> Jak widać spora część ruchu pochodzi z trzech pierwszych wyników wyszukiwania, dlatego bez wątpienia jest o co walczyć.
+				  	</p>
+				 </div>
+		   </div>
+			</div>
+      </div> <!-- === Section End === -->
+      <!-- === Section Three === -->
+      <div class="container mx-auto">
+       <div class="row">
+         <div class="col-12 seo-page-section">
+           <div class="center-content my-auto">
+            <h2 class="section-title font-kanit">Pozycjo<span class="one-letter">n</span>owanie w wyszukiwarce <span style="color: #4285f4;">G</span><span style="color: #d62d20;">o</span><span style="color: #ffa700;">o</span><span style="color: #4285f4;">g</span><span style="color: #008744;">l</span><span style="color: #d62d20;">e</span></h2>
+            <p class="pl-3 pr-3  font-merriweather">
+							Prowadząc każdy biznes ważne jest aby był on widoczny w internecie, zwłaszcza jeśli działasz w branży e-commerce i prowadzisz sklep internetowy. Większa widoczność w wyszukiwarkach pozwala użytkownikom dotrzeć do Twojej firmy i skorzystać z Twojej oferty. Bezwzględnym monopolistą na Polskim rynku wyszukiwarek jest Google. <strong>Pozycjonowanie strony w Google</strong> to na tą chwilę konieczność jeśli chcemy zaistnieć w internecie i rozwijać swój biznes.
+           </p>
+         </div>
+        </div>
+       </div>
+			 <div class="row">
+				<div class="col-lg-4 col-sm-12 card horizontal-align">
+					<div class="center-content my-auto">
+					 <h3 class="section-title font-kanit"><span class="one-letter">P</span>opularność wyszukiwarek w Polsce</h3>
+					 <hr class="line">
+						 <p class="pl-3 pr-3 font-merriweather">
+							 Zdecyodwanie w Polsce największą popularnością cieszy się wyszukiwarka Google. To właśnie tam najwięcej użytkowników szuka informacji o produktach, usługach, miejsach etc. Wysokie pozycje w innych wyszukiwarakch nie wiążą się z większymi korzyściami ponieważ w dużej mierze są one nawet nie znane przez internautów.
+						 </p>
+					</div>
+				</div>
+				<div class="col-lg-8 col-sm-12 card">
+					<img src="images/popularnosc wyszukiwarek w polsce.jpg" class="img-fluid lazyload" alt="pozycjonowanie strony w google" title="Popularność wyszukiwarek internetowych w Polsce">
+			 </div>
+			 </div>
+       </div> <!-- === Section End === -->
+			 <div class="row m-0 pt-5">
+				 <div class="col-12 form-section">
+					 <div class="d-flex justify-content-center mt-5 mb-2">
+					 <p>Chcesz zwiększyć widoczność swojej strony w wynikach wyszukiwania Google? Napisz do nas i zapytaj o usługi pozycjonowania!</p>
+					 </div>
+					 <a class="btn btn-form btn-lg mx-auto" aria-label="Wyceń wstępny koszt projektu" href="/koszt-strony-internetowej">Napisz do nas &raquo;</a>
+				 </div>
+			 <div class="col-12 contact-line" id="kontakt">
+				 <div class="row">
+						 <div class="col-sm-12 col-md-6 ">
+							 <div class="row align-middle info-section-1">
+							 <a href="tel:+48578356352" class="d-flex align-items-center" title="Skontaktuj się z nami pod numerem telefonu 578356352"><i class="fas fa-phone"></i> +48 578 356 352</a>
+						 </div>
+						 </div>
+						 <div class="col-sm-12 col-md-6">
+							 <div class="row align-middle info-section-2">
+							 <p class="contact-info d-flex align-items-center"><i class="fas fa-envelope"></i> office.rbcode@gmail.com </p>
+						 </div>
+						</div>
+				 </div>
+			 </div>
+		 </div>
+       <!-- === Section Four === -->
+       <div class="container mx-auto">
+        <div class="row">
+          <div class="col-12 seo-page-section">
+            <div class="center-content my-auto">
+             <h2 class="section-title font-kanit">S<span class="one-letter">t</span>rategie pozycjonowania stron www</h2>
+             <p class="pl-3 pr-3  font-merriweather">
+
+            </p>
+          </div>
+         </div>
+        </div>
+        </div> <!-- === Section End === -->
+        <!-- === Section Five === -->
+        <div class="container mx-auto">
+         <div class="row">
+           <div class="col-12 seo-page-section">
+             <div class="center-content my-auto">
+              <h2 class="section-title font-kanit">Eta<span class="one-letter">p</span>y pozycjonowania</h2>
+              <p class="pl-3 pr-3  font-merriweather">
+
+             </p>
+           </div>
+          </div>
+         </div>
+         </div> <!-- === Section End === -->
+         <!-- === Section Six === -->
+         <div class="container mx-auto">
+          <div class="row">
+            <div class="col-12 seo-page-section">
+              <div class="center-content my-auto">
+               <h2 class="section-title font-kanit"><span class="one-letter">J</span>ak mierzyć efekty pozycjonowania stron internetowych</h2>
+               <p class="pl-3 pr-3  font-merriweather">
+
+              </p>
+            </div>
+           </div>
+          </div>
+          </div> <!-- === Section End === -->
+  </div><!-- === Container End === -->
+
+<!-- === Seo Section === -->
+<div class="container mx-auto my-4">
+	<div class="row seo-section">
+		<div class="col-lg-8 col-sm-12 card">
+ 			<img src="images/seo.png" class="img-fluid lazyload" alt="Optymalizacja struktury strony zgodnie z SEO" title="Pozycjonowanie stron internetowych">
+ 	 </div>
+   <div class="col-lg-4 col-sm-12 card horizontal-align">
+		 <div class="center-content my-auto">
+	    <h2 class="section-title font-kanit"><span class="one-letter">P</span>ozycjonowanie stron interneto<span class="one-letter">w</span>ych</h2>
+			<hr class="line">
+		    <p class="pl-3 pr-3 font-merriweather"><strong>Skuteczne pozycjonowanie</strong> zapewnia wysoką pozycje w rankingach wyszukiwania powodując większą liczbę odwiedzin.
+				Tworząc struktury naszych stron stosujemy najlepsze praktyki i optymalizujemy nasz kod aby twoja <strong>strona internetowa</strong> była zgodna z zaleceniami SEO. Dzięki temu <strong>pozycjonowanie stron</strong> będzię skuteczne i pozwoli zwiększyć obroty.
+		  	</p>
+				<div class="button-position">
+					<a class="btn btn-offer btn-lg" aria-label="Pozycjonowanie stron internetowych - przejdz do oferty" href="#">Sprawdź ofertę &raquo;</a>
+				</div>
+		 </div>
+   </div>
+	</div>
+</div>  <!-- === Seo Section End === -->
+
+<!-- === Pricing Section === -->
+			<div class="row m-0">
+				<div class="col-12 form-section">
+					<div class="d-flex justify-content-center mt-5 mb-2">
+					<p>Chcesz zwiększyć widoczność swojej strony w wynikach wyszukiwania Google? Napisz do nas i zapytaj o usługi pozycjonowania!</p>
+					</div>
+					<a class="btn btn-form btn-lg mx-auto" aria-label="Wyceń wstępny koszt projektu" href="/koszt-strony-internetowej">Napisz do nas &raquo;</a>
+				</div>
+			<div class="col-12 contact-line" id="kontakt">
+				<div class="row">
+						<div class="col-sm-12 col-md-6 ">
+							<div class="row align-middle info-section-1">
+							<a href="tel:+48578356352" class="d-flex align-items-center" title="Skontaktuj się z nami pod numerem telefonu 578356352"><i class="fas fa-phone"></i> +48 578 356 352</a>
+						</div>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<div class="row align-middle info-section-2">
+							<p class="contact-info d-flex align-items-center"><i class="fas fa-envelope"></i> office.rbcode@gmail.com </p>
+						</div>
+					 </div>
+				</div>
+			</div>
+		</div> <!-- === Pricing Section === -->
+
+<?php
+include_once "includes/footer-inc.php";
+?>
+
+</body>
+</html>
